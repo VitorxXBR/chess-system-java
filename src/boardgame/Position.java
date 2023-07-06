dc7f751676aeda1a4e40a7131de0d11919d7198f
Vitor Rocha <vitor.vra@gmail.com>
2023-07-06 01:07:07 -0300

    First Class:  Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,45 @@
+package boardgame;
+
+public class Position {
+	
+	private Integer row;
+	private Integer column;
+	
+	//Construtores: 
+	public Position() {
+	}
+
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	//Getters and Setters
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	//toString
+	@Override
+	public String toString() {	
+		return row + ", "+column;
+	}
+	
+	
+	//Métodos: 
+	public void setValue(int row, int column) {
+		
+	}
+}
